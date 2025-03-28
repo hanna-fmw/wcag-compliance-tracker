b55d59def5ae7d8c951f3472485ea239eb8c1b45
Hanna Hosk <112755425+hanna-fmw@users.noreply.github.com>
2025-03-28 10:27:06 +0100

    Enhance NavigationTabs component by implementing dynamic tab activation based on the current pathname, improving user experience and navigation clarity.

--- app/components/NavigationTabs.js
+++ app/components/NavigationTabs.js
@@ -1,13 +1,33 @@
 'use client'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 
 export default function NavigationTabs() {
 	const router = useRouter()
+	const pathname = usePathname()
+
+	// Function to determine the active tab based on the current path
+	const getActiveTab = (path) => {
+		switch (path) {
+			case '/':
+				return 'instructions'
+			case '/basic-tests':
+				return 'basic'
+			case '/in-depth-tests':
+				return 'in-depth'
+			case '/test-tools':
+				return 'tools'
+			case '/useful-info':
+				return 'useful-info'
+			default:
+				return 'instructions'
+		}
+	}
+
 	return (
 		<main>
 			<div className='container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl py-4'>
-				<Tabs defaultValue='instructions' className='w-full'>
+				<Tabs value={getActiveTab(pathname)} className='w-full'>
 					<TabsList className='flex h-9 items-center justify-start rounded-lg bg-muted p-1 text-muted-foreground w-fit'>
 						<TabsTrigger
 							value='instructions'
